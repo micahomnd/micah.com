0f1123adf180f48d7772cee2a645abf594552dad
gl0git <williamcolinsgeorge@gmail.com>
2024-10-01 14:00:11 -0400

    temporarily remove project writings, and remove projects dir from navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -53,7 +53,7 @@ export default function Navbar() {
             </TooltipContent>
           </Tooltip>
         </DockIcon>
-        <DockIcon>
+        {/* <DockIcon>
           <Tooltip>
             <TooltipTrigger asChild>
               <Link
@@ -70,7 +70,7 @@ export default function Navbar() {
               <p>Projects</p>
             </TooltipContent>
           </Tooltip>
-        </DockIcon>
+        </DockIcon> */}
         <Separator orientation="vertical" className="h-full" />
         {Object.entries(DATA.contact.social).map(([name, social]) => (
           <DockIcon key={name}>
